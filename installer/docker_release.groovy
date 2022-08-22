5c2708a06c5d3ad99fef601ec8a10d7b0c97c239
lvfei.lv <lvfei.lv@alibaba-inc.com>
2022-08-22 20:16:39 +0800

    modify docker-release rules

--- installer/docker_release.groovy
+++ installer/docker_release.groovy
@@ -57,10 +57,18 @@ def replaceStrInDockerfile(absoluteFilePath, arch, os, version) {
   def content = readFile absoluteFilePath
   def baseName = absoluteFilePath.split("/")[-1]
   def newContent = ""
-  def flags = ["x86": false, "x64": false, "aarch64": false]
+  def flags = ["x86": false, "x64": false, "aarch64": false, "type": false, "match_type": false]
   for (line in content.split("\n")) {
     if (line.contains("ENV JAVA_VERSION")) {
       newContent += "ENV JAVA_VERSION ${version}\n"
+    } else if (line.contains("EDITION") && line.contains("=")) {
+      flags["type"] = true
+      if (params.TYPE == "extended")
+        flags["match_type"] = true
+      newContent += "${line}\n"
+    } else if (flags["type"] && !flags["match_type"] && params.TYPE == "standard" && line.contains("else")) {
+      flags["match_type"] = true
+      newContent += "${line}\n"
     } else if (line.contains(")") && line.contains("|")) {
       if (line.contains("aarch64") || line.contains("arm64"))
         flags["aarch64"] = true
@@ -69,30 +77,42 @@ def replaceStrInDockerfile(absoluteFilePath, arch, os, version) {
       else if (line.contains("x86") && !line.contains("x86-64"))
         flags["x86"] = true
       newContent += "${line}\n"
-    } else if (line.contains("ESUM=")) {
+    } else if (line.contains("ESUM=") && flags["match_type"]) {
+      def record = false
       for (e in flags) {
-        if (e.value) {
+        if (e.key != "type" && e.key != "match_type" && e.value) {
           def esum = line.split("'")[1]
           newContent += line.replace(esum, releaseMap[os].get(e.key).get("sha256")) + "\n"
+          record = true
           break
         }
       }
-    } else if (line.contains("BINARY_URL=")) {
+      if (!record)
+        newContent += "${line}\n"
+    } else if (line.contains("BINARY_URL=") && flags["match_type"]) {
+      def record = false
       for (e in flags) {
-        if (e.value) {
+        if (e.key != "type" && e.key != "match_type" && e.value) {
           def url = line.split("'")[1]
           newContent += line.replace(url, releaseMap[os].get(e.key).get("url")) + "\n"
+          record = true
           e.value = false
           break
         }
       }
+      if (!record)
+        newContent += "${line}\n"
     } else {
+      if (line.contains("fi \\")) {
+        flags["match_type"] = false
+        flags["type"] = false
+      }
       newContent += "${line}\n"
     }
   }
   writeFile file:absoluteFilePath, text: newContent, encoding: "UTF-8"
   sh "git diff"
-  sh "git add . && git commit -m 'Update ${platform} ${os} ${baseName}' && git push"
+  //sh "git add . && git commit -m 'Update ${platform} ${os} ${baseName}' && git push"
 }
 
 node('ossworker' && 'dockerfile') {
@@ -108,17 +128,22 @@ node('ossworker' && 'dockerfile') {
     sh(script: "docker run  registry.cn-hangzhou.aliyuncs.com/dragonwell/dragonwell:${tagName.replace("+", ".")}_x86_64_slim java -version 2> tmpt")
     def fullVersionOutput = sh(script: "cat tmpt", returnStdout: true).trim()
     def jdkVersion = fullVersionOutput.split("\n")[1].split("build ")[1].split("\\)")[0].trim()
+    def finalTag = jdkVersion
     if (params.RELEASE == "8") {
-      jdkVersion = (tagName.split("_")[-1].split("-")[0] + "-" + fullVersionOutput.split("-")[-1].split("\\)")[0]).trim()
+      jdkVersion = (tagName.split("_")[-1].split("-")[0] + "-" + fullVersionOutput.split("-")[-1].split("\\)")[0]).split(",")[0].trim()
+      finalTag = tagName.split("_")[0].split("-")[-1].trim()
     } else if (params.RELEASE == "11") {
       jdkVersion = "jdk-" + jdkVersion
+      finalTag = finalTag.replace("+", ".")
     } else if (params.RELEASE == "17") {
-      jdkVersion = tagName.split("_jdk")[0].split("-")[-1].trim()
+      jdkVersion = "jdk-" + tagName.split("_jdk")[0].split("-")[-1].trim()
+      finalTag = tagName.split("_jdk")[0].split("-")[-1].replace("+", ".").trim()
     }
     println """* release name: ${releaseName}
 * tag name: ${tagName}
 * jdk version: ${jdkVersion}
-* full version: ${fullVersionOutput}"""
+* full version: ${fullVersionOutput}
+* final tag: ${finalTag}"""
     for (asset in assets) {
       def assetName = asset.get("name")
       def assetDownloadUrl = asset.get("browser_download_url")
@@ -149,7 +174,7 @@ node('ossworker' && 'dockerfile') {
           def releaseVersion = splited[1]
           def releaseOS = splited[2]
           def fileBaseName = splited[-1]
-          if (fileBaseName.contains(params.TYPE) && releaseVersion == params.RELEASE) {
+          if (fileBaseName.contains("Dockerfile.releases") && releaseVersion == params.RELEASE) {
             def fileSuffix = fileBaseName.split("\\.")[-1]
             def arch = ""
             def os = releaseOS == "windows" ? releaseOS : releaseOS == "alpine" ? releaseOS : "linux"
@@ -182,7 +207,7 @@ node('ossworker' && 'dockerfile') {
       }
       writeFile file:"version", text: newVersions
       sh "git diff"
-      sh "git add . && git commit -m 'Update dragonwell${params.RELEASE}-${params.TYPE} version' && git push"
+      //sh "git add . && git commit -m 'Update dragonwell${params.RELEASE}-${params.TYPE} version' && git push"
     }
   } 
 }
