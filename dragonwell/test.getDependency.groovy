d0a746059b59d32a7789a3917d742c3c06997523
lvfei.lv <lvfei.lv@alibaba-inc.com>
2023-02-03 11:39:24 +0800

    add renaissance.jar to test.getDenpendency

--- dragonwell/test.getDependency.groovy
+++ dragonwell/test.getDependency.groovy
@@ -9,9 +9,10 @@ properties([
 ])
 
 node("perl") {
-  sh "rm -rf getDependencies.pl *.jar, *.zip, *.txt, *.gz"
+  sh "rm -rf getDependencies.pl *"
   sh "wget --no-check-certificate https://raw.githubusercontent.com/adoptium/TKG/master/scripts/getDependencies.pl"
   sh "perl ./getDependencies.pl -path . -task default"
   sh "curl -Lks -C - https://sourceforge.net/projects/dacapobench/files/latest/download -o dacapo.jar"
+  sh "curl -Lks -C - https://github.com/renaissance-benchmarks/renaissance/releases/download/v0.14.0/renaissance-mit-0.14.0.jar -o renaissance.jar"
   archiveArtifacts artifacts: "*.jar, *.zip, *.txt, *.gz", fingerprint: true, allowEmptyArchive: true
 }
