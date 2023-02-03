0d255df048a5b01a5046b6060c1accc60ae5a8b4
lvfei.lv <lvfei.lv@alibaba-inc.com>
2023-02-03 11:26:29 +0800

    add dacapo.jar

--- dragonwell/test.getDependency.groovy
+++ dragonwell/test.getDependency.groovy
@@ -9,8 +9,9 @@ properties([
 ])
 
 node("perl") {
-  sh "rm -rf getDependencies.pl"
+  sh "rm -rf getDependencies.pl *.jar, *.zip, *.txt, *.gz"
   sh "wget --no-check-certificate https://raw.githubusercontent.com/adoptium/TKG/master/scripts/getDependencies.pl"
   sh "perl ./getDependencies.pl -path . -task default"
+  sh "curl -Lks -C - https://sourceforge.net/projects/dacapobench/files/latest/download -o dacapo.jar"
   archiveArtifacts artifacts: "*.jar, *.zip, *.txt, *.gz", fingerprint: true, allowEmptyArchive: true
 }
