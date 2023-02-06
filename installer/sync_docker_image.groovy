6a08ba72456f7a62f8ac3137d2b30e18b29f91ab
lvfei.lv <lvfei.lv@alibaba-inc.com>
2023-02-06 11:40:51 +0800

    add retry in sync_docker_image

--- installer/sync_docker_image.groovy
+++ installer/sync_docker_image.groovy
@@ -4,7 +4,8 @@ node('ossworker && dockerfile && x64') {
   dir(env.WORKSPACE) {
     sh "cp ~/image-syncer ."
     timeout(time: 8, unit: 'HOURS') {
-      sh "./image-syncer --config=/root/.docker/sync_config.json"
+      // https://github.com/AliyunContainerService/image-syncer/blob/master/FAQs.md#how-can-i-deal-with-the-failed-sync-tasks%E5%90%8C%E6%AD%A5%E5%A4%B1%E8%B4%A5%E7%9A%84%E4%BB%BB%E5%8A%A1%E5%A6%82%E4%BD%95%E5%A4%84%E7%90%86
+     sh "./image-syncer --config=/root/.docker/sync_config.json --retries=3"
     }
   }
 }
