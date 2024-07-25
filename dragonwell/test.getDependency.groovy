2c4a7dbf87533668206bb5685bc9a7d5103a6859
lvfei.lv <lvfei.lv@alibaba-inc.com>
2024-07-25 16:42:14 +0800

    add NODE_LABLE in test.getDependency.groovy

--- dragonwell/test.getDependency.groovy
+++ dragonwell/test.getDependency.groovy
@@ -4,11 +4,20 @@ properties([
       defaultValue: "test",
       description: "BUILD_TYPE determines what type of build we are getting dependency jars for, e.g. BUILD_TYPE='system' will fetch dependency jars for system test builds, BUILD_TYPE='test' will fetch dependency jars for all test builds other than system",
       name: "BUILD_TYPE"
+    ),
+    string(
+      defaultValue: "",
+      name: "NODE_LABEL"
     )
   ])
 ])
 
-node("perl") {
+def nodeLabel = "perl"
+if (!params.NODE_LABEL.equals("")) {
+  nodeLabel += " && ${params.NODE_LABEL}"
+}
+
+node(nodeLabel) {
   sh "rm -rf getDependencies.pl *"
   sh "wget --no-check-certificate https://raw.githubusercontent.com/adoptium/TKG/master/scripts/getDependencies.pl"
   sh "perl ./getDependencies.pl -path . -task default"
